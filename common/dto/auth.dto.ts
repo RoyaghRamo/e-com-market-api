5193b7a21fe2caf0d1ca3585b5d37dc9a9c38ed6
ghayor.omar.97 <or.ghayor@gmail.com>
2023-05-01 01:22:09 +0100

    Refactoring

--- common/dto/auth.dto.ts
+++ common/dto/auth.dto.ts
@@ -3,18 +3,18 @@ import { IsEmail, IsNotEmpty, IsString, Matches } from 'class-validator';
 export const passwordPatternError =
   'Password must clear the following conditions:\n\t* at least 1 lower character\n\t* at least 1 upper character\n\t* at least 1 number\n\t* at least 1 special character\n\t* Minimum length is 8\n';
 
+export const passwordRegex =
+  /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&]{8,}$/;
+
 export class LoginDto {
   @IsString()
   @IsEmail()
   readonly email: string;
 
   @IsString()
-  @Matches(
-    /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&]{8,}$/,
-    {
-      message: passwordPatternError,
-    },
-  )
+  @Matches(passwordRegex, {
+    message: passwordPatternError,
+  })
   readonly password: string;
 }
 
@@ -32,12 +32,9 @@ export class RegisterDto {
   readonly email: string;
 
   @IsString()
-  @Matches(
-    /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&]{8,}$/,
-    {
-      message: passwordPatternError,
-    },
-  )
+  @Matches(passwordRegex, {
+    message: passwordPatternError,
+  })
   readonly password: string;
 
   @IsString()
